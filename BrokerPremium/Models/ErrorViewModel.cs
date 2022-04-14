dbe3f38dc0c29d662bc86073d2ba865b8261e094
Lubimka <Lubimka@DESKTOP-BTQ60D9>
2022-04-14 07:29:43 +0300

    Add project files.

---
+++ BrokerPremium/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrokerPremium.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
