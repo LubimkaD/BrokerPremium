afa97443d2a470081d897936f883dd4801667b4f
LubimkaD <lubi77ve@abv.bg>
2022-04-17 22:59:50 +0300

    Data moved to Infrastructure

--- BrokerPremium.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ BrokerPremium.Infrastructure/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using System;
 
-namespace BrokerPremium.Data.Migrations
+namespace BrokerPremium.Infrastructure.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
--- BrokerPremium/Program.cs
+++ BrokerPremium/Program.cs
@@ -1,5 +1,5 @@
 using BrokerPremium.Core.Constants;
-using BrokerPremium.Data;
+using BrokerPremium.Infrastructure.Data;
 using BrokerPremium.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -15,12 +15,12 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews()
     .AddMvcOptions(options =>
     {
