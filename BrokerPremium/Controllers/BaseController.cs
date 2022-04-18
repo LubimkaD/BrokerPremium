25acb61b5aa1ab824ebdef023dfb353db1680451
LubimkaD <lubi77ve@abv.bg>
2022-04-18 17:03:54 +0300

    Login, Register and Logout added

---
+++ BrokerPremium/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrokerPremium.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+        
+    }
+}
