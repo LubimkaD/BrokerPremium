1f0b3f1d0a135449544f21d9709462c59e9db7a4
LubimkaD <lubi77ve@abv.bg>
2022-04-20 01:22:05 +0300

    API aded

---
+++ BrokerPremium.API/Extensions/ApiServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using BrokerPremium.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Micrososft.Extensions.DependecyInjection
+{
+    public static class ApiServiceCollectionExtension
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            //services.AddScoped<IApplicationDbRepository ApplicationDbRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApiDbContexts(this IServiceCollection services, IConfiguration context)
+        {
+            var connectionString = context.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+            return services;
+        }
+    }
+}
+
+
