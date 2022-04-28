e5794eef50a775d824b2be43686c9279f3ef05d1
LubimkaD <lubi77ve@abv.bg>
2022-04-28 08:48:25 +0300

    UpdateCustomerTest added

--- BrokerPremium.Test/AddNewCustomerTest.cs
+++ BrokerPremium.Test/AddNewCustomerTest.cs
@@ -3,6 +3,7 @@ using BrokerPremium.Core.Models;
 using BrokerPremium.Core.Services;
 using BrokerPremium.Infrastructure.Data.Models;
 using BrokerPremium.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using System.Threading.Tasks;
@@ -47,6 +48,31 @@ namespace BrokerPremium.Test
 
             };
             Assert.IsTrue(await service.AddNewCustomer(newModelCustomer));
+        }
+
+        [Test]
+        public async Task AssertTestCustomerUpdated()
+        {
+            var service = serviceProvider.GetService<ICustomerService>();
+            var repo = serviceProvider.GetService<IApplicationDbRepository>();
+            var customer = await repo.All<Customer>()
+                .FirstOrDefaultAsync(c => c.Identificator == "111");
+            if (customer != null)
+            {
+                var newModelCustomer = new CustomerEditViewModel()
+                {
+                    Identificator = "111",
+                    Name = "testName2",
+                    Address = "testAddress2",
+                    IsCompany = true,
+                    Email = "testEmail2",
+                    Telefon = "testTelefon2",
+                    DateValidFrom = System.DateTime.Today
+
+                };
+                Assert.IsTrue(await service.UpdateCustomer(newModelCustomer));
+            }
+            
         }
 
         [TearDown]
